079739aabb18666a07f7fb5c8c4b0667a42966ff
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-01-20 10:25:40 +0100

    Raindrop Container - Implemented a container that can store raindrop values

---
+++ Oiski.School.RainStatistic_H2_2021.Application/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oiski.School.RainStatistic_H2_2021.Application
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            RainDropContainer calc = new RainDropContainer(5);
+
+            decimal[] values = new decimal[2];
+            for ( int i = 0; i < values.Length; i++ )
+            {
+                values[i] = i + 1;
+            }
+
+            calc.AddRange(6, values);
+
+            foreach ( var item in calc.ValuePool )
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
