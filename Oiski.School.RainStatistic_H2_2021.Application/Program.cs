39231a4ed7297a0f8cf4403aaec8c219cb4dbd67
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-01-20 23:10:54 +0100

    Results Menu - Implemented Results Menu with fields for displaying a table of contents along with Minimum, Maximum and Average values for that table.

--- Oiski.School.RainStatistic_H2_2021.Application/Program.cs
+++ Oiski.School.RainStatistic_H2_2021.Application/Program.cs
@@ -22,6 +22,13 @@ namespace Oiski.School.RainStatistic_H2_2021.Application
             Menu dataMenu = null;
             Menu resultMenu = null;
 
+            #region Calculation Values
+            ColorableListBox<decimal> table = null;
+            ColorableLabel minValue = null;
+            ColorableLabel maxValue = null;
+            ColorableLabel avgValue = null;
+            #endregion 
+
             RaindropContainer container = null;
             int valueCounter = 1;
 
@@ -73,11 +80,38 @@ namespace Oiski.School.RainStatistic_H2_2021.Application
                 #region Hide Main Menu
                 OiskiEngine.Input.ResetSlection();
                 OiskiEngine.Input.ResetInput();
+                s.BorderStyle(BorderArea.Horizontal, '-');
                 mainMenu.Show(false);
                 #endregion
 
-                dataMenu.Controls.GetSelectableControls[0].BorderStyle(BorderArea.Horizontal, '~');
-                //  Show Result Menu
+                #region Initialize Calculation Fields
+                if ( container != null )
+                {
+
+                    if ( table.Items.Count != 0 )
+                    {
+                        for ( int i = 0; i < table.Items.Count; i++ )
+                        {
+                            table.Items.RemoveItem(table.Items[i]);
+                        }
+                    }
+
+                    foreach ( decimal value in container.ValuePool )
+                    {
+                        table.Items.AddItem($"Value ({valueCounter}): {value}", value);
+                        valueCounter++;
+                    }
+
+                    valueCounter = 1;
+                    minValue.Text = container.GetMinimum().ToString();
+                    maxValue.Text = container.GetMaximum().ToString();
+                    avgValue.Text = container.GetAverage().ToString();
+                }
+                #endregion 
+
+                resultMenu.Controls.GetSelectableControls[0].BorderStyle(BorderArea.Horizontal, '~');
+                mainMenu.Controls.GetSelectableControls[0].BorderStyle(BorderArea.Horizontal, '~');
+                resultMenu.Show();
             };
 
             mainMenu.Controls.AddControl(toResultMenu);
@@ -195,7 +229,7 @@ namespace Oiski.School.RainStatistic_H2_2021.Application
 
                 if ( container != null )
                 {
-                    if ( decimal.TryParse(valueText.Text, out decimal _value) && valueCounter < container.ValuePool.Length )
+                    if ( decimal.TryParse(valueText.Text, out decimal _value) && valueCounter - 1 < container.ValuePool.Length )
                     {
                         container.ValuePool[valueCounter - 1] = _value;
                     }
@@ -259,7 +293,80 @@ namespace Oiski.School.RainStatistic_H2_2021.Application
             #endregion
 
             #region Result Menu
-            //  Establish Result Menu
+            resultMenu = new Menu();
+
+            #region Header
+            ColorableLabel resultMenuHeader = new ColorableLabel("Oiski's Results Section", textColor, borderColor, _attachToEngine: false);
+            resultMenuHeader.Position = new Vector2(Vector2.CenterX(resultMenuHeader.Size.x), 5);
+
+            resultMenu.Controls.AddControl(resultMenuHeader);
+            #endregion
+
+            #region Table
+            table = new ColorableListBox<decimal>("Raindrop Values", 10, 70, textColor, borderColor, _attachToEngine: false)
+            {
+                SelectedIndex = Vector2.Zero,
+                SelectableItems = true,
+                HighlightColor = new RenderColor(ConsoleColor.Black, ConsoleColor.Gray)
+            };
+
+            resultMenu.Controls.AddControl(table);
+            #endregion
+
+            #region Calculations
+            #region Minimum
+            ColorableLabel minLabel = new ColorableLabel("Minimum Value", textColor, borderColor, _attachToEngine: false);
+            minLabel.Position = new Vector2(20, table.Position.y + table.Size.y + 2);
+            resultMenu.Controls.AddControl(minLabel);
+
+            minValue = new ColorableLabel("Min", new RenderColor(ConsoleColor.Green, ConsoleColor.Black), borderColor, _attachToEngine: false);
+            minValue.Position = new Vector2(minLabel.Position.x + minLabel.Size.x - 1, minLabel.Position.y);
+            resultMenu.Controls.AddControl(minValue);
+            #endregion
+
+            #region Maximum
+            ColorableLabel maxLabel = new ColorableLabel("Maximum Value", textColor, borderColor, _attachToEngine: false);
+            maxLabel.Position = new Vector2(20, minLabel.Position.y + minLabel.Size.y + 2);
+            resultMenu.Controls.AddControl(maxLabel);
+
+            maxValue = new ColorableLabel("Max", new RenderColor(ConsoleColor.Green, ConsoleColor.Black), borderColor, _attachToEngine: false);
+            maxValue.Position = new Vector2(maxLabel.Position.x + maxLabel.Size.x - 1, maxLabel.Position.y);
+            resultMenu.Controls.AddControl(maxValue);
+            #endregion
+
+            #region Average
+            ColorableLabel avgLabel = new ColorableLabel("Average Value", textColor, borderColor, _attachToEngine: false);
+            avgLabel.Position = new Vector2(20, maxLabel.Position.y + maxLabel.Size.y + 2);
+            resultMenu.Controls.AddControl(avgLabel);
+
+            avgValue = new ColorableLabel("Avg", new RenderColor(ConsoleColor.Green, ConsoleColor.Black), borderColor, _attachToEngine: false);
+            avgValue.Position = new Vector2(avgLabel.Position.x + avgLabel.Size.x - 1, avgLabel.Position.y);
+            resultMenu.Controls.AddControl(avgValue);
+            #endregion
+            #endregion
+
+            #region Back Button
+            ColorableOption resultMenuBack = new ColorableOption("Back", textColor, borderColor, _attachToEngine: false)
+            {
+                SelectedIndex = new Vector2(0, 1)
+            };
+            resultMenuBack.Position = new Vector2(Vector2.CenterX(dataMenuBack.Size.x), avgLabel.Position.y + 6);
+
+            resultMenuBack.OnSelect += (s) =>
+            {
+                #region Hide Result Menu
+                OiskiEngine.Input.ResetInput();
+                OiskiEngine.Input.ResetSlection();
+                resultMenu.Show(false);
+                #endregion
+
+                valueCounter = 1;
+                s.BorderStyle(BorderArea.Horizontal, '-');
+                mainMenu.Show();
+            };
+
+            resultMenu.Controls.AddControl(resultMenuBack);
+            #endregion
             #endregion
         }
     }
